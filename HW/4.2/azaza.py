6360506ca5e600984b3f25b06941102e5973af62
Jem Maratov <dzhemshit.maratov@itechart-group.com>
2022-04-27 23:52:19 +0300

    4.3: finished HW

--- HW/4.2/azaza.py
+++ HW/4.2/azaza.py
@@ -1,6 +1,8 @@
 import socket
 from time import sleep
 import os
+import json
+import yaml
 
 names = ['drive.google.com', 'mail.google.com', 'google.com']
 
@@ -11,7 +13,15 @@ def getDNSResolution(names):
         addrs[name] = addr
     return addrs
 
+def saveDNSResolution(records):
+    with open('dnsResolution.json', 'w') as jsonFile:
+        jsonFile.write(str(json.dumps(records)))
+    with open('dnsResolution.yaml', 'w') as yamlFile:
+        yamlFile.write(yaml.dump(records))
+    return
+
 records = getDNSResolution(names)
+saveDNSResolution(records)
 
 while True:
     print('\n')
@@ -23,3 +33,4 @@ while True:
         else:
             print(' [ERROR] ' + name + ' IP mistmatch: ' + records[name] + '=>' + addr)
             records[name] = addr
+            saveDNSResolution(records)
